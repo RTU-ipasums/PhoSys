b275b92d657e22e5b02ed7893c5f96a874a7a473
sshsphere <47260097+sshsphere@users.noreply.github.com>
2023-03-21 22:26:45 +0200

    Change selectedShapes to Set

--- frontend/src/Draw.vue
+++ frontend/src/Draw.vue
@@ -7,7 +7,7 @@ export default {
       currentShapeId: 0,
       data,
       copiedObject: null,
-      selectedShapes: [],
+      selectedShapes: new Set(),
       lastCenter: null,
       lastDist: 0,
       stageConfig: {
@@ -131,7 +131,7 @@ export default {
 
       selectedShape.x = e.target.x();
       selectedShape.y = e.target.y();
-    
+
     },
     handleTransformEnd(e) {
       const name = e.target.name();
@@ -153,8 +153,9 @@ export default {
       const name = e.target.name();
       const shape = this.data.shapes.find((r) => r.name === name);
       if (shape) {
-        if (!e.evt.shiftKey && !this.selectedShapes.includes(shape)) {
-          this.selectedShapes = [shape];
+        if (!e.evt.shiftKey && !this.selectedShapes.has(shape)) {
+          this.selectedShapes.clear();
+          this.selectedShapes.add(shape);
         }
       }
       this.updateTransformer();
@@ -162,7 +163,7 @@ export default {
     handleStageClick(e) {
       // clicked on stage - clear selection
       if (e.target === e.target.getStage()) {
-        this.selectedShapes = [];
+        this.selectedShapes.clear();
         this.updateTransformer();
         return;
       }
@@ -176,20 +177,19 @@ export default {
       const name = e.target.name();
       const shape = this.data.shapes.find((r) => r.name === name);
       if (!shape) {
-        this.selectedShapes = [];
-      } 
+        this.selectedShapes.clear();
+      }
       else {
         if (e.evt.shiftKey) {
-          const index = this.selectedShapes.indexOf(shape);
-          if (index === -1) {
-            this.selectedShapes.push(shape);
-          } 
-          else {
-            this.selectedShapes.splice(index, 1);
+          if (!this.selectedShapes.has(shape)) {
+            this.selectedShapes.add(shape);
+          } else {
+            this.selectedShapes.delete(shape);
           }
-        } 
+        }
         else {
-          this.selectedShapes = [shape];
+          this.selectedShapes.clear();
+          this.selectedShapes.add(shape);
         }
       }
       this.updateTransformer();
@@ -199,9 +199,9 @@ export default {
       const transformerNode = this.$refs.transformer.getNode();
       const stage = transformerNode.getStage();
       let selectedNodes = [];
-      this.selectedShapes.forEach((obj) => {
-        selectedNodes.push(stage.findOne('.' + obj.name));
-      });
+      for (const selectedShape of this.selectedShapes) {
+        selectedNodes.push(stage.findOne('.' + selectedShape.name));
+      }
       if (selectedNodes && selectedNodes.length !== 0) {
         // attach to another node
         transformerNode.nodes(selectedNodes);
@@ -217,17 +217,13 @@ export default {
         ...newObject(obj),
         name: `${type}_${this.currentShapeId}`
       })
-      this.selectedShapes = [this.data.shapes.at(-1)];
+      this.selectedShapes.clear();
+      this.selectedShapes.add(this.data.shapes.at(-1));
       Promise.resolve(this.selectedShapes).then(this.updateTransformer);
     },
     deleteSelectedShapes(obj) {
-      //Is there a more efficient way?
-      this.selectedShapes.forEach((selectedShape) => {
-        this.data.shapes = this.data.shapes.filter((shape) => {
-          return shape.name !== selectedShape.name;
-        });
-      })
-      this.selectedShapes = [];
+      this.data.shapes.filter((shape) => !selectedShapes.has(shape));
+      this.selectedShapes.clear();
       this.updateTransformer();
     }
   },
@@ -244,24 +240,24 @@ export default {
           this.deleteSelectedShapes();
           break;
         case "ArrowLeft":
-          this.selectedShapes.forEach((obj) => {
-            obj.x=Math.floor(obj.x-0.0001);
-          });
+          for (const obj of this.selectedShapes) {
+            obj.x = Math.floor(obj.x - 0.0001);
+          };
           break;
         case "ArrowUp":
-          this.selectedShapes.forEach((obj) => {
-            obj.y=Math.floor(obj.y-0.0001);
-          });
+          for (const obj of this.selectedShapes) {
+            obj.y = Math.floor(obj.y - 0.0001);
+          };
           break;
         case "ArrowRight":
-          this.selectedShapes.forEach((obj) => {
-              obj.x=Math.ceil(obj.x+0.0001);
-          });
+          for (const obj of this.selectedShapes) {
+            obj.x = Math.ceil(obj.x + 0.0001);
+          };
           break;
         case "ArrowDown":
-          this.selectedShapes.forEach((obj) => {
-            obj.y=Math.ceil(obj.y+0.0001);
-          });
+          for (const obj of this.selectedShapes) {
+            obj.y = Math.ceil(obj.y + 0.0001);
+          };
           break;
         case "c":
           if (!e.ctrlKey || this.selectedShapes.length === 0) break;
@@ -272,6 +268,7 @@ export default {
           this.copiedObjects.forEach((obj) => {
             obj.x += 10;
             obj.y += 10;
+            //Why not push objects directly to the shapes array?
             this.addShape(obj, obj.name.split('_')[0]);
           })
       }
@@ -283,8 +280,8 @@ export default {
   <!-- export addrect, import RECT ARRAY from other files -->
   <div>
     <v-stage ref="stage" :config="stageConfig" @mousedown="handleStageMouseDown" @touchstart="handleStageMouseDown"
-      @click="handleStageClick" @dragstart="handleDragstart" @dragend="handleDragend" @touchmove="handleTouch" @touchend="handleTouchEnd"
-      @wheel="zoomStage" @keydown.delete="deleteSelectedShapes">
+      @click="handleStageClick" @dragstart="handleDragstart" @dragend="handleDragend" @touchmove="handleTouch"
+      @touchend="handleTouchEnd" @wheel="zoomStage" @keydown.delete="deleteSelectedShapes">
       <v-layer ref="layer">
         <v-rect :config="{
           x: 0,
